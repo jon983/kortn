521813d15b779f574666d979dbfecf58256b42c5
jon983 <jon@jonads.com>
2026-09-19 00:25:22 +1000

    fix(kalooki): remove unused makeRng import from cards test

--- lib/kalooki/__tests__/cards.test.ts
+++ lib/kalooki/__tests__/cards.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { makeDeck, meldPoints, makeRng, shuffle } from '../cards';
+import { makeDeck, meldPoints, shuffle } from '../cards';
 import { makeRng as rngFromRng } from '../rng';
 
 describe('makeDeck', () => {
